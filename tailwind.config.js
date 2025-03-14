b962a3e047783d03770dad3ee998ce3ca988c826
Gaurav Pandey <gauravp5923@gmail.com>
2025-03-14 11:10:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    darkMode: "class", // Enable dark mode using a class
+    content: ["./index.html", "./About.html", "./contact.html", "./gallery.html", "/review.html", "/blog.html",], // Update paths to your HTML files
+    theme: {
+        extend: {},
+    }, plugins: [],
+};
